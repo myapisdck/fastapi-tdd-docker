8388829bc0e6caa9b16ec65657d6ef2f36383ae0
seldeclerck <seldeclerck@gmail.com>
2022-07-16 17:06:11 +0200

    Added Text Summarization

--- project/tests/test_summaries_unit.py
+++ project/tests/test_summaries_unit.py
@@ -2,9 +2,8 @@ import json
 from datetime import datetime
 
 import pytest
-from project.app.api import summaries
 
-from app.api import crud
+from app.api import crud, summaries
 
 
 def test_create_summary(test_app, monkeypatch):
